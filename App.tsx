6ab2dd4e554dd14c1ad4522c6adf33f2815427d2
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2023-02-14 15:39:52 -0400

    feat: base project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "@components/Loading";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import Groups from "./src/screens/Groups";
+import theme from "@theme/index";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Groups />
+      {/* <Loading /> */}
+    </ThemeProvider>
+  );
+  // return (
+  // <ThemeProvider theme={theme}>
+  //     {fontsLoaded ? <Groups /> : <Loading />}
+  // </ThemeProvider>
+  // );
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+const Groups = () => {
+  return <Container></Container>;
+};
+
+export default Groups;
