ad2e7d747c5b228b90d465fc0241f98dd5a08b24
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2023-02-14 17:47:14 -0400

    feat: create Header and Highlight components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,15 @@
+import Header from "@components/Header";
+import Highlight from "@components/Highlight";
 import { Container } from "./styles";
 
 const Groups = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+    </Container>
+  );
 };
 
 export default Groups;
