41ff1a9a6a79bf86efd58b9a2018f31a57f82691
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2023-02-15 10:53:49 -0400

    feat: create GroupCard and EmptyList components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,32 @@
+import GroupCard from "@components/GroupCard";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
+import ListEmpty from "@components/ListEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 const Groups = () => {
+  const [groups, setGroups] = useState([
+    "Galera da Dom",
+    "povo da da da da da da da da da da da da da da da da da",
+    "galera da igreja",
+  ]);
+
   return (
     <Container>
       <Header showBackButton />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 };
